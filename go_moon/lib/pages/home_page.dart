337a6afe66d27eb8d44dc9ac1f050769e9aed680
shivam <shivamcse7056@gmail.com>
2025-04-17 13:19:23 +0530

    Basic Update

--- go_moon/lib/pages/home_page.dart
+++ go_moon/lib/pages/home_page.dart
@@ -1,19 +1,53 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  late double _deviceHeight, _deviceWidth;
+
+  HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/moon.png"),
-            fit: BoxFit.contain,
-          ),
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: _deviceWidth * 0.05),
+          height: _deviceHeight,
+          width: _deviceWidth,
+          child: _pageTitle(),
         ),
       ),
     );
+  }
+
+  Widget _pageTitle() {
+    return const Text(
+      "#GoMoon",
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 70,
+        fontWeight: FontWeight.w800,
+      ),
+    );
+  }
+
+  Widget _astroImageWidget() {
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/images/moon.png"),
+          fit: BoxFit.contain,
+        ),
+      ),
+    );
+  }
+
+  Widget _destinationDropDownWidget() {
+    List<DropDownMenuItem> _items =
+        ['James Webb', 'Shivam Tiwari', 'Shivam Shukla'].map((e) {
+          return DropDownMenuItem(child: Text(e), value: e);
+        }).toList();
+    return Container(child: DropDownButton(onChange: (_) {}, items: _items));
   }
 }
